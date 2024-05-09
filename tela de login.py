b1f6264359c365b050c789aff0550e04126a053b
Matheus Ribeiro <162144183+Falklives@users.noreply.github.com>
2024-05-09 10:02:53 -0300

    Update tela de login.py

--- tela de login.py
+++ tela de login.py
@@ -4,37 +4,25 @@ from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 
-
-class Login(BoxLayout):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.orientation = "vertical"
-        self.padding = [50, 50]
-
-        
-        self.add_widget(Label(text="LOGIN", font_size=30))
-
-        
-        self.username_input = TextInput(hint_text="Nome de usuário ...")
-        self.senha_input = TextInput(hint_text="Digite sua senha ...", password=True)
-        
-
-        self.add_widget(Label(text="Nome de usuário:"))
-        self.add_widget(self.username_input)
-        self.add_widget(Label(text="Senha:"))
-        self.add_widget(self.senha_input)
-        
-        
-
-       
-        self.cadastrar_button = Button(text="Cadastrar", background_color=(0, 0, 128))
-        self.login_button = Button(text="Já possuo uma conta", background_color=(255, 0, 0))
-        self.add_widget(self.cadastrar_button)
-        self.add_widget(self.login_button)
-
 class MyApp(App):
     def build(self):
-        return Login()
+        layout = BoxLayout(orientation = "vertical", padding = [45, 45])
+        title = Label(text = 'LOGIN', font_size = 50)
+        layout.add_widget(title)
+        title_usuario = Label(text = 'Nome de usuário:')
+        layout.add_widget(title_usuario)
+        caixa_usuario = TextInput(hint_text = 'Digite seu usuário...')
+        layout.add_widget(caixa_usuario)
+        title_senha =  Label(text = 'Senha:')
+        layout.add_widget(title_senha)
+        caixa_senha =  TextInput(hint_text = 'Digite sua senha...')
+        layout.add_widget(caixa_senha)
+        botao = Button(text = 'Fazer Login', background_color = (0, 0, 128))
+        layout.add_widget(botao)
+        botao2 = Button(text = 'Já possuo uma conta', background_color = (255, 0, 0))
+        layout.add_widget(botao2)
 
+        return layout
+        
 if __name__ == '__main__':
     MyApp().run()
